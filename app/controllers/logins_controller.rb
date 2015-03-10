4fad4bac3bb8247247319993c4cab2ea074666fc
Jonathan Parrish <yoda.peh@gmail.com>
2015-03-10 15:43:40 -0400

    Add login code and correct routes

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,10 +1,22 @@
 class LoginsController < ApplicationController
   def new
+
   end
 
   def create
+    developer = Developer.find_by_email(params[:email])
+    if developer && developer.authenticate(params[:password])
+      session[:developer_id] = developer.id
+      flash[:notice] = "Welcome"
+      redirect_to developers_path
+    else
+      flash.now[:notice] = "please re-enter your info."
+      render :new
+    end
   end
 
   def destroy
+    session[:developer_id] = nil
+    redirect_to login_path
   end
 end
