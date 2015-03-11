8aac807c6a9c99861e1a7800f099a5ed641c003e
Jonathan Parrish <yoda.peh@gmail.com>
2015-03-11 16:47:21 -0400

    Add red feature to Projects

--- test/controllers/developers_controller_test.rb
+++ test/controllers/developers_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class DevelopersControllerTest < ActionController::TestCase
   setup do
     @developer = developers(:one)
+    session[:developer_id] = @developer_id
   end
 
   test "should get developer index" do
