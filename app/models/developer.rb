3a9ef63b240d005ecbb7db2ddf00a84d615182d0
Jonathan Parrish <yoda.peh@gmail.com>
2015-03-11 18:21:43 -0400

    Add relationship dependencies project and developer

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -4,31 +4,13 @@ class Developer < ActiveRecord::Base
   validates :password, :length => { :minimum=> 6}
   validates_confirmation_of :password
 
-  has_many :time_entries
+  has_many :time_entries, dependent: :restrict_with_exception
   has_secure_password
-
-  # def project_total_hours(project)
-  #   entries = self.project_time_entries(project)
-  #   total = 0
-  #   entries.each do |e|
-  #     total += e.hours
-  #   end
-  #   return total
-  # end
 
   def total_hours
     self.time_entries.reduce(0.0) do |sum, e|
       sum + e.hours
     end
   end
-
-  # def color_change
-  #   if total_hours > 40
-  #
-  #
-  # end
-  # private def project_time_entries(project)
-  #   self.time_entries.where(project_id: project.id)
-  # end
 
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
-  has_many :time_entries
+  has_many :time_entries, dependent: :restrict_with_exception
+  has_many :developers, through: :time_entries
 
   def project_total_hours
     total = 0
