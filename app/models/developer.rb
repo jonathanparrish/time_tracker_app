b57a4546c6e6ebe448d83bca9a449fce21680199
Jonathan Parrish <yoda.peh@gmail.com>
2015-03-12 19:30:48 -0400

    Fix password validation

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,8 +1,14 @@
 class Developer < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, :length => { :minimum=> 6}
-  validates_confirmation_of :password
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:minimum => 6},
+                       :on => :create
+  validates :password, :confirmation => true,
+                       :length => {:minimum => 6},
+                       :allow_blank => true,
+                       :on => :update
 
   has_many :time_entries, dependent: :restrict_with_error
   has_secure_password
